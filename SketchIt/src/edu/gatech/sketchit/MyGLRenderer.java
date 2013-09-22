f14aadae24f0baee434178f6366166199914d217
Thomas Shields <me@thomasshields.net>
2013-09-21 20:26:10 -0400

    zooooooom

--- SketchIt/src/edu/gatech/sketchit/MyGLRenderer.java
+++ SketchIt/src/edu/gatech/sketchit/MyGLRenderer.java
@@ -48,6 +48,15 @@ public class MyGLRenderer implements Renderer {
 	
 	public float mAngleX;
 	public float mAngleY;
+	public float zoom;
+	
+	private int lastWidth;
+	private int lastHeight;
+	
+	public void zoom(float z) {
+		zoom += z;
+		onSurfaceChanged(null, lastWidth, lastHeight);
+	}
 	
 	private void draw(Shape s) {
 		s.vertexBuffer.position(mPositionOffset);
@@ -96,6 +105,8 @@ public class MyGLRenderer implements Renderer {
 
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		lastWidth = width;
+		lastHeight = height;
 		GLES20.glViewport(0, 0, width, height);
 
 		// Create a new perspective projection matrix. The height will stay the same
@@ -105,8 +116,8 @@ public class MyGLRenderer implements Renderer {
 		final float right = ratio;
 		final float bottom = -1.0f;
 		final float top = 1.0f;
-		final float near = 1.0f;
-		final float far = 10.0f;
+		final float near = 1.0f + zoom*.001f;
+		final float far = 8.0f + zoom*0.1f;
 		
 		Matrix.frustumM(mProjectionMatrix, 0, left, right, bottom, top, near, far);
 	}
--- SketchIt/src/edu/gatech/sketchit/activities/SketchActivity.java
+++ SketchIt/src/edu/gatech/sketchit/activities/SketchActivity.java
@@ -80,7 +80,7 @@ public class SketchActivity extends Activity implements CvCameraViewListener2{
 
 		mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.gl_screen_view);
 		mOpenCvCameraView.setCvCameraViewListener(this);
-		mGLView = new AGLSurfaceView(this);
+		mGLView = new MyGLSurfaceView(this);
 		addContentView(mGLView,new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT));
 	
 	}
@@ -131,15 +131,16 @@ public class SketchActivity extends Activity implements CvCameraViewListener2{
 	}
 }
 
-class AGLSurfaceView extends GLSurfaceView {
+class MyGLSurfaceView extends GLSurfaceView {
 	private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
 	private float mPreviousX;
 	private float mPreviousY;
 	private float mPreviousZ;
 	private final MyGLRenderer mRenderer;
 	private boolean generated;
+	private boolean zoomMode= true;
 
-	public AGLSurfaceView(Context context) {
+	public MyGLSurfaceView(Context context) {
 		super(context);
 		setEGLContextClientVersion(2);
 		super.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
@@ -148,7 +149,11 @@ class AGLSurfaceView extends GLSurfaceView {
 		setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
 
 	}
-
+	
+	public void setZoomMode(boolean on) {
+		zoomMode = on;
+	}
+	
 	private void generate() {
 		System.out.println("rendering");
 //		Point3[] rand = Shape.randomShape(4);
@@ -156,12 +161,12 @@ class AGLSurfaceView extends GLSurfaceView {
 //		mRenderer.addShape(r);
 //		requestRender();
 
-//		for(int i=0;i<50;i++) {
-//			Point3[] rand = Shape.randomShape(2);
-//			Shape r = new Line(rand[0], rand[1]);
-//			mRenderer.addShape(r);
-//		}
-//		requestRender();
+		for(int i=0;i<50;i++) {
+			Point3[] rand = Shape.randomShape(2);
+			Shape r = new Line(rand[0], rand[1]);
+			mRenderer.addShape(r);
+		}
+		requestRender();
 //		for(int i=0;i<5;i++) {
 //			Point3[] rand = Shape.randomShape(4);
 //			Shape r = new Rectangle(rand[0], rand[1], rand[2], rand[3]);
@@ -190,12 +195,16 @@ class AGLSurfaceView extends GLSurfaceView {
 
 			float dx = x - mPreviousX;
 			float dy = y - mPreviousY;
-
-                mRenderer.mAngleX += (dx) * TOUCH_SCALE_FACTOR; 
-                mRenderer.mAngleY += (dy) * TOUCH_SCALE_FACTOR;  // = 180.0f / 320
-
-	            requestRender();
-	        }
+			
+			if(zoomMode) {
+				mRenderer.zoom(dy);
+			}
+			else {
+	            mRenderer.mAngleX += (dx) * TOUCH_SCALE_FACTOR; 
+	            mRenderer.mAngleY += (dy) * TOUCH_SCALE_FACTOR;  // = 180.0f / 320
+			}
+            requestRender();
+	   }
 
 
 		mPreviousX = x;
